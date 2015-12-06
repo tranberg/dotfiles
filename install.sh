9956f82ca5f1037786f500687f63f52fd6117fda
Bo Tranberg <bo@tberg.dk>
2015-12-06 20:48:00 +0100

    install gruvbox color scheme

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 # Install script for dotfiles. Places symlinks in home folder to track updates
 
+echo "Installing dotfiles"
+
 # Full path to dotfiles
 path=$(pwd)
 
@@ -11,3 +13,9 @@ for file in .vimrc .tmux.conf
 do
     cp -vs $path/$file .
 done
+
+# Install gruvbox color scheme for vim
+echo "Installing gruvbox color scheme for vim"
+mkdir -p .vim/colors
+cd .vim/colors
+wget https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim
