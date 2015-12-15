c96e850a938470ff173970679beb3632c6f239f2
Bo Tranberg <bo@dashsoft.dk>
2015-12-15 12:50:21 +0100

    overwrite old files

--- install.sh
+++ install.sh
@@ -11,11 +11,12 @@ cd ~/
 # Create symlinks to dotfiles
 for file in .vimrc .tmux.conf
 do
+    rm $file
     cp -vs $path/$file .
 done
 
 # Install gruvbox color scheme for vim
 echo "Installing gruvbox color scheme for vim"
 mkdir -p .vim/colors
-cd .vim/colors
-wget https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim
+wget -q https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim -O .vim/colors/gruvbox.vim
+echo "Done"
